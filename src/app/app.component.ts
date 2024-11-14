149e32ea7c81f5b2e0f8fc4a469abffbf76facf9
Pradip patel <pradip.patel@samyak.com>
2024-11-14 17:36:12 +0530

    push new changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,28 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatCardModule } from '@angular/material/card'
+import { HomeComponent } from "./home/home.component";
+import { CommonModule } from '@angular/common';
+import { ReversePipe } from './custom/reverse.pipe';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatCardModule, HomeComponent, CommonModule, ReversePipe, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Angular 18 Standalone';
+  title = 'Angular 18 Tutorial';
+  subtitle = 'Angular for bigginers';
+  todaydate = new Date();
+  salary = 1000;
+  obj = {'name':'pradip'};
+
+  isDisabled = false;
+
+  ChangeTitle(){
+    this.title = 'Angular 18 For Tutorial';
+  }
 }
