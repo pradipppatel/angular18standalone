d3b9dbbde68fe0b054d2e46e2f3225e8095680c9
Pradip patel <pradip.patel@samyak.com>
2024-11-15 14:16:00 +0530

    push new changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,15 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { MatCardModule } from '@angular/material/card'
-import { HomeComponent } from "./home/home.component";
-import { CommonModule } from '@angular/common';
-import { ReversePipe } from './custom/reverse.pipe';
-import { FormsModule } from '@angular/forms';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatCardModule, HomeComponent, CommonModule, ReversePipe, FormsModule],
+  imports: [RouterOutlet, RouterLink, MatToolbarModule, MatIconModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Angular 18 Tutorial';
-  subtitle = 'Angular for bigginers';
-  todaydate = new Date();
-  salary = 1000;
-  obj = {'name':'pradip'};
-
-  isDisabled = false;
-
-  ChangeTitle(){
-    this.title = 'Angular 18 For Tutorial';
-  }
+  
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card'
+import { CommonModule } from '@angular/common';
+import { ReversePipe } from '../custom/reverse.pipe';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule, CommonModule, ReversePipe, FormsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+
+  title = 'Angular 18 Tutorial';
+  subtitle = 'Angular for bigginers';
+  todaydate = new Date();
+  salary = 1000;
+  obj = {'name':'pradip'};
+
+  isDisabled = false;
+
+  ChangeTitle(){
+    this.title = 'Angular 18 For Tutorial';
+  }
 
 }
